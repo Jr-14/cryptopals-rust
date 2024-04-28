e389153be75f8fd8a0d7758732b19392d124f1bb
John Emerson Requizo <john.requizo@doshii.io>
2024-04-28 17:56:41 +1000

    Add convert to hex

--- notes/set1.md
+++ notes/set1.md
@@ -1,1 +1,60 @@
 # Set 1
+
+# Convert hex to base64
+## What is hex and base64?
+Hex (short for Hexadecimal, Base 16) is a way to represent numbers. We've standardised our number representation to use Decimal
+(Base 10), meaning we represent one digit with 10 different symbols. In Hexadecimal a digit is represented by 16 different
+symbols (Symbols are 0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f where a represents the Decimal number 10 and f represents the
+decimal number 15).
+
+Likewise Base64 is a way to represent numbers; where one digit can be represented by 64 different symbols. Here are the
+full length of Base64 digits from lowest to highest where 'padding' digit is represented as '='.
+```
+0  = A      16 = Q      32 = g      48 = w      padding = 
+1  = B      17 = R      33 = h      49 = x
+2  = C      18 = S      34 = i      50 = y
+3  = D      19 = T      35 = j      51 = z
+4  = E      20 = U      36 = k      52 = 0
+5  = F      21 = V      37 = l      53 = 1
+6  = G      22 = W      38 = m      54 = 2
+7  = H      23 = X      39 = n      55 = 3
+8  = I      24 = Y      40 = o      56 = 4
+9  = J      25 = Z      41 = p      57 = 5
+10 = K      26 = a      42 = q      58 = 6
+11 = L      27 = b      43 = r      59 = 7
+12 = M      28 = c      44 = s      60 = 8
+13 = N      29 = d      45 = t      61 = 9
+14 = O      30 = e      46 = u      62 = +
+15 = P      31 = f      47 = v      63 = /
+```
+
+Notice that with Base64 the letters are case sensitive where as with Hexadecimal we can represent letters 'a' to 'f' as
+either lowercase or uppercase.
+
+## Hexadecimal and Base64 representation in Binary
+Fortunately Hexadecimal and Base64 is a factor of 2 :) Notice that to represent all Hexadecimal symbols we will require
+16 symbols, but we only have 2 symbols in Binary. Therefore to represent a single Hexadecimal symbol, we require 4 bits
+max.
+
+```
+0000 = 0
+0001 = 1
+0010 = 2
+0011 = 3
+0100 = 4
+0101 = 5
+0110 = 6
+0111 = 7
+1000 = 8
+1001 = 9
+1010 = a = 10 (in base10)
+1011 = b = 11 (in base10)
+1100 = c = 12 (in base10)
+1101 = d = 13 (in base10)
+1110 = e = 14 (in base10)
+1111 = f = 15 (in base10)
+```
+
+Perfect! We can do the same with Base64. We require 64 Symbols but only have 2 symbols. How many bits do we require?
+$x = log_2(64) \therefore x = 6$ Therefore we require 6 bits!
+
--- src/set1.rs
+++ src/set1.rs
@@ -8,7 +8,7 @@ fn get_str_len(s: &str) {
     println!("String length is {}", s.chars().count());
 }
 
-fn print_str_to_bits(s: &str) {
+fn print_str_to_bits(s: &str){
     let bit_str: Vec<_> = s
         .as_bytes()
         .iter()
@@ -16,32 +16,113 @@ fn print_str_to_bits(s: &str) {
             let f = format!("{c:b}");
             let padding = 8 - f.chars().count();
             assert!(padding >= 0);
-            f
+            "0".repeat(padding).to_string() + &f
         })
         .collect();
     println!("{:?}", bit_str);
 }
 
+fn decode_hex_to_base64(v: Vec<u8>) -> Vec<u8> {
+    let c: Vec<u8> = v
+        .iter()
+        .zip([3, 2, 1, 0])
+        .map(|(vc, num)| {
+            println!("Val: {}, iter {}", vc, num);
+            vc.clone()
+        })
+        .collect();
+    println!("{:?}", c);
+    c
+}
+
+fn print_vec_u8(v: Vec<u8>) {
+    
+}
+
+/// Calculate the corresponding hex character into a number
+fn hex_value(c: u8) -> Result<u8, &'static str> {
+    match c {
+        b'a'..=b'f' => Ok(c - b'a' + 10),
+        b'A'..=b'F' => Ok(c - b'A' + 10),
+        b'0'..=b'9' => Ok(c -b'0'),
+        _ => Err("Invalid hex character")
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_hex_to_base64() {
         let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         assert_eq!(s, hex_to_base64(s));
     }
 
     #[test]
+    #[ignore]
     fn test_str_length() {
         let r = "abcd";
         print_str_to_bits(&r);
     }
 
     #[test]
+    #[ignore]
     fn print_another_string() {
         let s = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
         print_str_to_bits(&s);
     }
+
+    #[test]
+    #[ignore]
+    fn test_print_char() {
+        let c = "c";
+        print_str_to_bits(&c);
+    }
+
+    #[test]
+    fn test_hex_value_lowercase() {
+        let hex_chars = "0123456789abcdef";
+        let vals = Vec::from([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+        let hex_vals: Vec<u8> = hex_chars
+            .as_bytes()
+            .iter()
+            .map(|b| hex_value(*b).unwrap())
+            .collect();
+        assert_eq!(vals, hex_vals);
+    }
+
+    #[test]
+    fn test_hex_value_uppercase() {
+        let hex_chars = "0123456789ABCDEF";
+        let vals = Vec::from([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+        let hex_vals: Vec<u8> = hex_chars
+            .as_bytes()
+            .iter()
+            .map(|b| hex_value(*b).unwrap())
+            .collect();
+        assert_eq!(vals, hex_vals);
+    }
+
+    #[test]
+    fn test_calc_hex() {
+        let hex_string = "fa7b";
+        let hex_iter = hex_string
+            .as_bytes()
+            .iter()
+            .map(|b| hex_value(*b).unwrap());
+        // let sum: u32 = hex_iter.fold(0u32, |sum ,i| sum + (i.clone() as u32));
+        hex_iter.for_each(|b| {
+            println!("{:08b}", b);
+        });
+    }
+
+    // #[test]
+    // fn test_decode_hex() {
+    //     // let v = Vec::from([10, 20, 30, 40]);
+    //     let v = Vec::from("0123456789abcdef".as_bytes());
+    //     v.chunks(4).for_each(|c| decode_hex_to_base64(c.to_vec()));
+    //     decode_hex_to_base64(v);
+    // }
 }
