2fd3a7f597a0a91457ba5d024b29904554b205f3
John Emerson Requizo <john.requizo@doshii.io>
2024-05-08 12:58:25 +1000

    Add completed hex to base 64 conversion

--- src/set1.rs
+++ src/set1.rs
@@ -1,51 +1,65 @@
-pub fn hex_to_base64(s: &str) -> String {
-    let ss = s.as_bytes();
-    println!("{:?}", ss);
-    s.to_string()
-}
-
-fn get_str_len(s: &str) {
-    println!("String length is {}", s.chars().count());
-}
-
-fn print_str_to_bits(s: &str){
-    let bit_str: Vec<_> = s
+pub fn hex_to_base64(hex_string: &str) -> String {
+    let base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/+";
+    hex_string
         .as_bytes()
         .iter()
-        .map(|&c| {
-            let f = format!("{c:b}");
-            let padding = 8 - f.chars().count();
-            assert!(padding >= 0);
-            "0".repeat(padding).to_string() + &f
+        .map(|byte| {
+            match hex_value(byte) {
+                Ok(val) => val,
+                _ => panic!("Invalid hex character"),
+            }
         })
-        .collect();
-    println!("{:?}", bit_str);
-}
-
-fn decode_hex_to_base64(v: Vec<u8>) -> Vec<u8> {
-    let c: Vec<u8> = v
-        .iter()
-        .zip([3, 2, 1, 0])
-        .map(|(vc, num)| {
-            println!("Val: {}, iter {}", vc, num);
-            vc.clone()
+        .collect::<Vec<u8>>()
+        .chunks(3)
+        .fold(String::from(""), |mut acc, chunk| {
+            if chunk.len() == 3 {
+                let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
+                let second_index: usize = (((chunk[1] & 3) << 4) + (chunk[2])).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                let second_char = match base64_chars.chars().nth(second_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push(second_char);
+            } else if chunk.len() == 2 {
+                let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
+                let second_index: usize = (((chunk[1] & 3) << 4)).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                let second_char = match base64_chars.chars().nth(second_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push(second_char);
+                acc.push('=');
+            } else {
+                let first_index: usize = ((chunk[0] << 2)).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push('=');
+                acc.push('=');
+            }
+            acc
         })
-        .collect();
-    println!("{:?}", c);
-    c
-}
-
-fn print_vec_u8(v: Vec<u8>) {
-    
 }
 
 /// Calculate the corresponding hex character into a number
-fn hex_value(c: u8) -> Result<u8, &'static str> {
+fn hex_value(c: &u8) -> Result<u8, &'static str> {
     match c {
         b'a'..=b'f' => Ok(c - b'a' + 10),
         b'A'..=b'F' => Ok(c - b'A' + 10),
-        b'0'..=b'9' => Ok(c -b'0'),
-        _ => Err("Invalid hex character")
+        b'0'..=b'9' => Ok(c - b'0'),
+        _ => Err("Invalid hex character"),
     }
 }
 
@@ -54,41 +68,13 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
-    fn test_hex_to_base64() {
-        let s = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        assert_eq!(s, hex_to_base64(s));
-    }
-
-    #[test]
-    #[ignore]
-    fn test_str_length() {
-        let r = "abcd";
-        print_str_to_bits(&r);
-    }
-
-    #[test]
-    #[ignore]
-    fn print_another_string() {
-        let s = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-        print_str_to_bits(&s);
-    }
-
-    #[test]
-    #[ignore]
-    fn test_print_char() {
-        let c = "c";
-        print_str_to_bits(&c);
-    }
-
-    #[test]
     fn test_hex_value_lowercase() {
         let hex_chars = "0123456789abcdef";
         let vals = Vec::from([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
         let hex_vals: Vec<u8> = hex_chars
             .as_bytes()
             .iter()
-            .map(|b| hex_value(*b).unwrap())
+            .map(|b| hex_value(b).unwrap())
             .collect();
         assert_eq!(vals, hex_vals);
     }
@@ -100,29 +86,22 @@ mod tests {
         let hex_vals: Vec<u8> = hex_chars
             .as_bytes()
             .iter()
-            .map(|b| hex_value(*b).unwrap())
+            .map(|b| hex_value(b).unwrap())
             .collect();
         assert_eq!(vals, hex_vals);
     }
 
     #[test]
-    fn test_calc_hex() {
-        let hex_string = "fa7b";
-        let hex_iter = hex_string
-            .as_bytes()
-            .iter()
-            .map(|b| hex_value(*b).unwrap());
-        // let sum: u32 = hex_iter.fold(0u32, |sum ,i| sum + (i.clone() as u32));
-        hex_iter.for_each(|b| {
-            println!("{:08b}", b);
-        });
+    fn test_hex_to_base64() {
+        let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     }
 
-    // #[test]
-    // fn test_decode_hex() {
-    //     // let v = Vec::from([10, 20, 30, 40]);
-    //     let v = Vec::from("0123456789abcdef".as_bytes());
-    //     v.chunks(4).for_each(|c| decode_hex_to_base64(c.to_vec()));
-    //     decode_hex_to_base64(v);
-    // }
+    #[test]
+    fn test_hex_to_base64_2() {
+        let hex_string = "ffff";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "++8==");
+    }
 }
