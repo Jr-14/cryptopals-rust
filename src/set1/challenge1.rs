7422338b1469d4dd37490e3ebfddf0e5da812516
John Emerson Requizo <john.requizo@doshii.io>
2024-05-09 14:17:27 +1000

    Refactor to put sets and challenges into correct modules

---
+++ src/set1/challenge1.rs
@@ -0,0 +1,108 @@
+pub fn hex_to_base64(hex_string: &str) -> String {
+    let base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    hex_string
+        .as_bytes()
+        .iter()
+        .map(|byte| match hex_value(byte) {
+            Ok(val) => val,
+            _ => panic!("Invalid hex character"),
+        })
+        .collect::<Vec<u8>>()
+        .chunks(3)
+        .fold(String::from(""), |mut acc, chunk| {
+            if chunk.len() == 3 {
+                let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
+                let second_index: usize = (((chunk[1] & 3) << 4) + (chunk[2])).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                let second_char = match base64_chars.chars().nth(second_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push(second_char);
+            } else if chunk.len() == 2 {
+                let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
+                let second_index: usize = ((chunk[1] & 3) << 4).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                let second_char = match base64_chars.chars().nth(second_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push(second_char);
+                acc.push('=');
+            } else {
+                let first_index: usize = (chunk[0] << 2).into();
+                let first_char = match base64_chars.chars().nth(first_index) {
+                    Some(c) => c,
+                    None => panic!("No base64 char found"),
+                };
+                acc.push(first_char);
+                acc.push('=');
+                acc.push('=');
+            }
+            acc
+        })
+}
+
+/// Calculate the corresponding hex character into a number
+pub fn hex_value(c: &u8) -> Result<u8, &'static str> {
+    match c {
+        b'a'..=b'f' => Ok(c - b'a' + 10),
+        b'A'..=b'F' => Ok(c - b'A' + 10),
+        b'0'..=b'9' => Ok(c - b'0'),
+        _ => Err("Invalid hex character"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hex_value_lowercase() {
+        let hex_chars = "0123456789abcdef";
+        let vals = Vec::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        let hex_vals: Vec<u8> = hex_chars
+            .as_bytes()
+            .iter()
+            .map(|b| hex_value(b).unwrap())
+            .collect();
+        assert_eq!(vals, hex_vals);
+    }
+
+    #[test]
+    fn test_hex_value_uppercase() {
+        let hex_chars = "0123456789ABCDEF";
+        let vals = Vec::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        let hex_vals: Vec<u8> = hex_chars
+            .as_bytes()
+            .iter()
+            .map(|b| hex_value(b).unwrap())
+            .collect();
+        assert_eq!(vals, hex_vals);
+    }
+
+    #[test]
+    fn test_hex_to_base64() {
+        let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(
+            base64_string,
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+
+    #[test]
+    fn test_hex_to_base64_2() {
+        let hex_string = "ffff";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "//8==");
+    }
+}
---
+++ src/set1/challenge2.rs
@@ -0,0 +1,34 @@
+use super::challenge1;
+use std::iter::zip;
+
+pub fn fixed_xor(hex_string_one: &str, hex_string_two: &str) -> String {
+    let hex_strings = "0123456789abcdef";
+    let string_one_bits: Vec<u8> = hex_string_one
+        .as_bytes()
+        .iter()
+        .map(|byte| challenge1::hex_value(byte).unwrap())
+        .collect();
+    let string_two_bits: Vec<u8> = hex_string_two
+        .as_bytes()
+        .iter()
+        .map(|byte| challenge1::hex_value(byte).unwrap())
+        .collect();
+
+    zip(string_one_bits, string_two_bits)
+        .map(|(x, y)| hex_strings.chars().nth((x ^ y) as usize).unwrap())
+        .fold(String::from(""), |acc, value| acc + &value.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::fixed_xor;
+
+    #[test]
+    fn test_fixed_xor() {
+        let xor_val = fixed_xor(
+            "1c0111001f010100061a024b53535009181c",
+            "686974207468652062756c6c277320657965",
+        );
+        assert_eq!(xor_val, "746865206b696420646f6e277420706c6179");
+    }
+}
