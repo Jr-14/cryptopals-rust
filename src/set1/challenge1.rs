f65527845c8060270afc5373933a7574d78a3de2
John Emerson Requizo <58794199+Jr-14@users.noreply.github.com>
2024-06-09 21:37:07 +1000

    Set 1 - Challenge 1 hex to base64 with padding (#3)
    * Add attempt at fixing padding
    
    * Complete addition of padding
    
    * Move padding to last bit of function
    
    * Cleanup PR

--- src/set1/challenge1.rs
+++ src/set1/challenge1.rs
@@ -1,54 +1,57 @@
 pub fn hex_to_base64(hex_string: &str) -> String {
     let base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    hex_string
+    let mut base64_str = hex_string
         .as_bytes()
         .iter()
-        .map(|byte| match hex_value(byte) {
-            Ok(val) => val,
-            _ => panic!("Invalid hex character"),
-        })
+        .map(|byte| hex_value(byte).expect("Invalid hex character"))
         .collect::<Vec<u8>>()
         .chunks(3)
         .fold(String::from(""), |mut acc, chunk| {
             if chunk.len() == 3 {
                 let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
                 let second_index: usize = (((chunk[1] & 3) << 4) + (chunk[2])).into();
-                let first_char = match base64_chars.chars().nth(first_index) {
-                    Some(c) => c,
-                    None => panic!("No base64 char found"),
-                };
-                let second_char = match base64_chars.chars().nth(second_index) {
-                    Some(c) => c,
-                    None => panic!("No base64 char found"),
-                };
+                let first_char = base64_chars
+                    .chars()
+                    .nth(first_index)
+                    .expect("No base64 char found");
+                let second_char = base64_chars
+                    .chars()
+                    .nth(second_index)
+                    .expect("No bas64 char found");
                 acc.push(first_char);
                 acc.push(second_char);
             } else if chunk.len() == 2 {
                 let first_index: usize = ((chunk[0] << 2) + (chunk[1] >> 2)).into();
                 let second_index: usize = ((chunk[1] & 3) << 4).into();
-                let first_char = match base64_chars.chars().nth(first_index) {
-                    Some(c) => c,
-                    None => panic!("No base64 char found"),
-                };
-                let second_char = match base64_chars.chars().nth(second_index) {
-                    Some(c) => c,
-                    None => panic!("No base64 char found"),
-                };
+                let first_char = base64_chars
+                    .chars()
+                    .nth(first_index) 
+                    .expect("No base64 char found");
+                let second_char = base64_chars
+                    .chars()
+                    .nth(second_index)
+                    .expect("No base64 char found");
                 acc.push(first_char);
                 acc.push(second_char);
-                acc.push('=');
             } else {
                 let first_index: usize = (chunk[0] << 2).into();
-                let first_char = match base64_chars.chars().nth(first_index) {
-                    Some(c) => c,
-                    None => panic!("No base64 char found"),
-                };
+                let first_char = base64_chars
+                    .chars()
+                    .nth(first_index) 
+                    .expect("No base64 char found");
                 acc.push(first_char);
-                acc.push('=');
-                acc.push('=');
             }
             acc
-        })
+        });
+    let padding_amount = if base64_str.len() % 4 == 0 {
+        0
+    } else {
+        4 - (base64_str.len() % 4)
+    };
+    for _ in 0..padding_amount {
+        base64_str.push('=');
+    }
+    base64_str
 }
 
 /// Calculate the corresponding hex character into a number
@@ -68,7 +71,7 @@ mod tests {
     #[test]
     fn test_hex_value_lowercase() {
         let hex_chars = "0123456789abcdef";
-        let vals = Vec::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        let vals: Vec<u8> = (0..=15).collect();
         let hex_vals: Vec<u8> = hex_chars
             .as_bytes()
             .iter()
@@ -80,7 +83,7 @@ mod tests {
     #[test]
     fn test_hex_value_uppercase() {
         let hex_chars = "0123456789ABCDEF";
-        let vals = Vec::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+        let vals: Vec<u8> = (0..=15).collect();
         let hex_vals: Vec<u8> = hex_chars
             .as_bytes()
             .iter()
@@ -97,12 +100,21 @@ mod tests {
             base64_string,
             "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
         );
-    }
-
-    #[test]
-    fn test_hex_to_base64_2() {
+        
         let hex_string = "ffff";
         let base64_string = hex_to_base64(hex_string);
-        assert_eq!(base64_string, "//8==");
+        assert_eq!(base64_string, "//8=");
+        
+        let hex_string = "4927";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "SSc=");
+
+        let hex_string = "49276d206b696";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "SSdtIGtpY===");
+
+        let hex_string = "fafa";
+        let base64_string = hex_to_base64(hex_string);
+        assert_eq!(base64_string, "+vo=");
     }
 }
